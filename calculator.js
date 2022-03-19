38dc5d0031d4f9dbf4636fbcd0294ddc5936a00e
borobor <bornajurisa@gmail.com>
2022-03-19 23:19:28 +0100

    Write code for addition

--- calculator.js
+++ calculator.js
@@ -1,24 +1,31 @@
-let displayCurrentNumber = document.querySelector('.second-row');
+let displayCurrentNumber = document.querySelector(".second-row");
+let displayLoggedNumber = document.querySelector(".first-row");
 
-let keyPad = document.querySelectorAll('button');
+let keyPad = document.querySelectorAll("button");
 
 let currentNumber = '';
+let loggedNumber = '';
 
-keyPad.forEach(key => {
-	key.addEventListener('click', () => {
-	keyClicked = key.textContent;
-	currentNumber += keyClicked
-	displayCurrentNumber.textContent += keyClicked;
-	calculate();
-	});
+keyPad.forEach((key) => {
+  key.addEventListener("click", () => {
+    keyClicked = key.textContent;
+    calculate();
+  });
 });
 
-let calculate = function(e) {
-	if (keyClicked == 'AC') {
-		currentNumber = '';
-		displayCurrentNumber.textContent = '';
+let calculate = function (e) {
+	if (keyClicked >= 0 && keyClicked <= 9) {
+		currentNumber += keyClicked;
+		displayCurrentNumber.textContent = currentNumber;
+	}
+  if (keyClicked == "AC") {
+    currentNumber = "";
+    displayCurrentNumber.textContent = "";
+  }
+	if (keyClicked == '+') {
+		loggedNumber = +loggedNumber + +currentNumber;
+		currentNumber = "";
+		displayCurrentNumber.textContent = "";
+		displayLoggedNumber.textContent = loggedNumber;
 	}
 };
-
-
-
